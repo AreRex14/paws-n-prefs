76ac9cf9b173a925b0ed8aa872eefd1a2296796d
Arif <rexcode14@gmail.com>
2025-08-29 03:38:24 +0800

    swap gh-pages with vite-plugin-gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import { ghPages } from 'vite-plugin-gh-pages'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    ghPages({
+      branch: 'gh-pages',
+      message: 'chore: deploy to gh-pages',
+      push: !!process.env.DEPLOY,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
